87f62a18be56b25b6a3d17358dc1fe26736783f8
Marcus Robb <marcus.robb@initworx.com>
2018-04-13 19:56:59 -0700

    First working version with backups enabled.

--- greenplum_admin/greenplum_backup.py
+++ greenplum_admin/greenplum_backup.py
@@ -12,20 +12,29 @@ import click
 import logging
 import ConfigParser
 import psycopg2
+import re
 from datetime import datetime
+from collections import defaultdict
+from subprocess import call
+from shutil import rmtree
 
 
 class Backup:
 
     def __init__(self, config, debug=None):
+
         loglevel=logging.ERROR
 
         self.debug = debug
         self.config = click.format_filename(config)
         self.logger = logging.getLogger(__name__)
+        self.logger.propagate = 0
 
         if self.debug:
             loglevel=logging.DEBUG
+
+        """ Load the config file """
+        self.configs = self.readconfig(self.config)
 
         self.setup_logging(loglevel)
 
@@ -33,17 +42,34 @@ class Backup:
         """
         Sets the basic config for the logger.
         """
-        logformat = "[%(levelname)s]:%(asctime)s - %(message)s"
-        logging.basicConfig(level=loglevel, stream=sys.stdout,
-                            format=logformat, datefmt="%Y-%m-%d %H:%M%S")
 
-    def readconfig(self, file):
+        _log_dir = self.configs.get('backup', 'log_directory')
+        logging.basicConfig(level=loglevel)
+
+        _logformatter = logging.Formatter("[%(levelname)s]:%(asctime)s - %(message)s")
+        _rootlogger = self.logger
+
+        if not _rootlogger.handlers:
+            if _log_dir:
+                _stamp = datetime.now().strftime('%Y%m%d%H%M%S')
+                _file_name = os.path.basename(sys.argv[0])
+                _log_file = '{}/{}_{}.log'.format(_log_dir, _file_name, _stamp)
+
+                filehandler = logging.FileHandler(_log_file)
+                filehandler.setFormatter(_logformatter)
+                _rootlogger.addHandler(filehandler)
+
+            consolehandler = logging.StreamHandler()
+            consolehandler.setFormatter(_logformatter)
+            _rootlogger.addHandler(consolehandler)
+
+    def readconfig(self, config_file):
         """
         Reads the config file
 
         Parameters
         ----------
-        file : string
+        config_file : string
             Description the relative path to the config file.
 
         Returns
@@ -53,26 +79,22 @@ class Backup:
 
         self.logger.debug('Reading config file: {}'.format(file))
         config = ConfigParser.ConfigParser()
-        config.read(file)
+        config.read(config_file)
 
         return config
 
-    def get_database_list(self, configs):
+    def get_database_list(self):
         """
         Used to get a dynamic list of databases on the cluster that are allowed connections.
 
-        Parameters
-        ----------
-        configs : instance of ConfigParser
-            Description all the config values in or configuration file.
 
         Returns
         -------
         list
         """
         _return_records=[]
-        _backup_user_db = configs.get('backup','backup_user_db')
-        _connection_db = configs.get('backup','connection_db')
+        _backup_user_db = self.configs.get('backup', 'backup_user_db')
+        _connection_db = self.configs.get('backup', 'connection_db')
         connection_string="host=localhost dbname={} user={}".format(_connection_db,_backup_user_db)
         try:
             conn = psycopg2.connect(connection_string)
@@ -87,14 +109,12 @@ class Backup:
 
         return _return_records
 
-    def build_backup_command(self,configs, db_list):
+    def build_backup_command(self, db_list):
         """
         Builds a list of backup commands that will be executed.
 
         Parameters
         ----------
-        configs : instance of ConfigParser
-            Description all the config values in or configuration file.
         db_list
             Description either the dynamic list of databases on the cluster or from the config file.
 
@@ -102,13 +122,13 @@ class Backup:
         -------
         list
         """
-        command_list=[]
+        command_list = []
         _today = datetime.now().strftime('%a')
         self.logger.debug('Day: {}'.format(_today).lower())
-        _options = configs.get('backup','backup_options')
-        _master_data_dir = configs.get('backup','greenplum_master_directory')
-        _backup_path = configs.get('backup','backup_path')
-        _backup_day = configs.get('backup', _today)
+        _options = self.configs.get('backup', 'backup_options')
+        _master_data_dir = self.configs.get('backup', 'greenplum_master_directory')
+        _backup_path = self.configs.get('backup', 'backup_path')
+        _backup_day = self.configs.get('backup', _today)
 
         if _backup_day != 'none':
             for db in db_list:
@@ -118,27 +138,161 @@ class Backup:
                 else:
                     _backup_type = _backup_day
 
-                cmd = 'gpcrondump -x {} {} -d {} -l {}/logs -u {} {}'.format(db,_options,_master_data_dir,_backup_folder,_backup_folder,_backup_type)
-                command_list.append({_backup_folder:cmd})
+                cmd = ' -x {} {} -d {} -l {}/logs -u {} {}'.format(db, _options, _master_data_dir,
+                                                                   _backup_folder, _backup_folder,
+                                                                   _backup_type)
+                command_list.append({_backup_folder: cmd})
 
         return command_list
 
-    def build_backup_dir(self, directory):
+    @staticmethod
+    def build_backup_dir(directory):
         if not os.path.isdir(directory):
             os.makedirs(directory)
+            return False
+        else:
+            return True
 
-    def execute_backups(self,backup_commands):
+    def execute_backups(self, backup_commands):
         for item in backup_commands:
-            dir = item.keys()[0]
-            cmd = item[dir]
-            self.build_backup_dir(dir)
+            _dir = item.keys()[0]
+            _cmd = item[_dir]
+
+            """ Append the log directory to our built path and create the new structure. """
+            _dir = '{}/{}'.format(_dir, 'logs')
+
+            _backup_program = self.configs.get('backup', 'backup_program')
+            if not self.build_backup_dir(_dir):
+                """ Backup directory was just created, we should over-ride incremental backups and run a full
+                backup or the backup will fail."""
+                _cmd = str(_cmd).replace('incremental', '')
+                self.logger.debug('Backup command: {}{}'.format(_backup_program, _cmd))
+            else:
+                self.logger.debug('Backup command: {}{}'.format(_backup_program, _cmd))
+
+            """ Execute the backup """
+            try:
+                call([_backup_program, _cmd])
+            except OSError as e:
+                self.logger.error('Failed to execute the backup. {}'.format(e.strerror))
+                sys.exit(-1)
+
+            """ Read the gp log files for this database and get information rotation cleanup."""
+            _dict_logs = self.read_gp_log(_dir)
+
+            """ Find the directories that are older than then number of full backups we keep """
+            _remaining = []
+            for key in _dict_logs:
+                self.logger.debug("Database log contents: Key {} ".format(key))
+                _counter = int(self.configs.get('backup', 'keep_full_backups'))
+                _oldest_full = []
+
+                for x in _dict_logs[key]:
+                    if str(x[2]).lower() == "full database":
+                        _counter -= 1
+                        if _counter == 0:
+                            _oldest_full.append(x)
+
+                        if _counter < 0:
+                            _remaining.append(x)
+
+                    elif _counter == 0:
+                        _remaining.append(x)
+
+                self.logger.debug("Oldest full: {}".format(_oldest_full))
+                self.logger.debug("Remaining: {}".format(_remaining))
+
+                """ delete old backups """
+                self.delete_old_backups(key, _remaining)
+
+    def delete_old_backups(self, database, del_list):
+        """
+        Deletes the old database backup folders and removes the corresponding log file.
+        Errors are non-fatal, we don't want to stop backups if we can't remove a directory.
+        Monitoring should pick up that the backup drive is filling up.
+
+        Parameters
+        ----------
+        database : string
+            Description name of the database to delete old files for
+
+        del_list : list
+            Description list of directory names and log files to delete
+
+        Returns
+        -------
+        bool
+        """
+        self.logger.debug("Delete List: {}".format(del_list))
+        for x in del_list:
+            _del_log = x[0]
+            _del_dir = '{}/{}/db_dumps/{}'.format(self.configs.get('backup', 'backup_path'), database, x[1])
+
+            if os.path.exists(_del_dir):
+                try:
+                    rmtree(_del_dir)
+                    self.logger.debug('Deleted directory: {}'.format(_del_dir))
+
+                    if os.path.exists(_del_log):
+                        try:
+                            os.remove(_del_log)
+                            self.logger.debug('Deleted log file: {}'.format(_del_log))
+                        except OSError as e:
+                            self.logger.error(
+                                'Error deleting log file: {}. Error: {} {}'.format(_del_log, e.errno, e.strerror))
+
+                except OSError as e:
+                    self.logger.error('Error deleting directory: {}. Error: {} {}'.format(_del_dir, e.errno, e.strerror))
+            else:
+                self.logger.warning('Backup folder does not exist but log file does!:  {} {}'.format(_del_dir, _del_log))
+
+    def read_gp_log(self, log_folder_path):
+        """
+        Parse the backup logs to find the type of backup and backup folder.
+        The backup folder is always automatically named by gpcrondump with the date the backup was executed;
+        this is useful for determining folders to clean up.
+
+        Parameters
+        ----------
+        log_folder_path : string
+            Description path for logs in each database backup directory
+
+        Returns
+        -------
+        defaultdict(list)
+        """
+        _dict = defaultdict(list)
+
+        _file_list = os.listdir(log_folder_path)
+        _find_db_name = re.compile(r"(Target database.*)")
+        _find_dump_dir = re.compile(r"(Dump subdirectory.*)")
+        _find_dump_type1 = re.compile(r"(Dump type.*)")
+        _find_dump_type2 = re.compile(r"(=.*)")
+        self.logger.debug("List of log files: {}".format(_file_list.sort(reverse=True)))
+        for fname in _file_list:
+            fullpath = '{}/{}'.format(log_folder_path, fname)
+            with open(fullpath, 'r') as f:
+                content = f.read()
+                f.close()
+                _db_name = str(_find_db_name.findall(content)[-1]).split()[-1]
+                _dump_dir = str(_find_dump_dir.findall(content)[-1]).split()[-1]
+                _dump_type1 = _find_dump_type1.findall(content)[-1]
+                _dump_type2 = _find_dump_type2.search(_dump_type1).group(1).replace('= ', '').strip()
+
+                self.logger.debug("Database: {}".format(_db_name))
+                self.logger.debug("Dump Dir: {}".format(_dump_dir))
+                self.logger.debug("Dump Type: {}".format(_dump_type1))
+                self.logger.debug("Dump Type: {}".format(_dump_type2))
+
+                _dict[_db_name].append((fullpath, _dump_dir, _dump_type2))
+
+        return _dict
 
     def run(self):
         """ Primary method to control program flow """
-        configs = self.readconfig(self.config)
-        _backup_user = configs.get('backup','backup_user_os')
-        _backup_path = configs.get('backup', 'backup_path')
-        _backup_database_list = map(str.strip,configs.get('backup','backup_database_list').split(','))
+        _backup_user = self.configs.get('backup', 'backup_user_os')
+        _backup_path = self.configs.get('backup', 'backup_path')
+        _backup_database_list = map(str.strip, self.configs.get('backup', 'backup_database_list').split(','))
 
         """ Must run with the user we're expecting """
         if not _backup_user == pwd.getpwuid(os.getuid())[0]:
@@ -153,13 +307,13 @@ class Backup:
 
             """ Get our database list either from the config file or from the cluster itself """
             if not len(_backup_database_list) > 0 or '' in _backup_database_list[:1]:
-                dbs = self.get_database_list(configs)
+                dbs = self.get_database_list()
             else:
                 dbs = _backup_database_list
             self.logger.debug(list(dbs))
 
             """ Generate the backup commands for gpcrondump """
-            backup_cmds = self.build_backup_command(configs,dbs)
+            backup_cmds = self.build_backup_command(dbs)
             self.logger.debug('Backup commands: {} '.format(backup_cmds))
 
             """ Execute backups """
@@ -173,8 +327,8 @@ class Backup:
 @click.command()
 @click.option('--config', '-c', required=True, help='Path to a configuration file.', type=click.Path(exists=True))
 @click.option('--debug', '-v', flag_value='DEBUG', help='Verbose logging output')
-def main(config,debug):
-    prog = Backup(config,debug)
+def main(config, debug):
+    prog = Backup(config, debug)
     prog.run()
 
 
